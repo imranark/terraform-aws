0aeec942c6381779b307e75ea891cc988720123d
Imran Kazi <imranarkazi@gmail.com>
2023-01-02 23:04:33 -0500

    key pairs

--- backend-s3.tf
+++ backend-s3.tf
@@ -1,7 +1,7 @@
 terraform {
- backend "s3" {
- bucket = "terra-state-vprofile101" # replace with your s3 bucketname
- key = "terraform/backend"
- region = "us-east-1"
- }
+  backend "s3" {
+    bucket = "terra-state-vprofile101" # replace with your s3 bucketname
+    key    = "terraform/backend"
+    region = "us-east-1"
+  }
 }
